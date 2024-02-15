9ee25d1b15b00d21c3f8a0ce2dc16b0b32875319
PatelPriyanshu-24 <159017504+PatelPriyanshu-24@users.noreply.github.com>
2024-02-15 17:18:39 +0530

    Update userdashboard.component.ts

--- src/app/component/userdashboard/userdashboard.component.ts
+++ src/app/component/userdashboard/userdashboard.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/shared/auth.service';
-
+import { PlatformLocation } from '@angular/common';
 @Component({
   selector: 'app-userdashboard',
   templateUrl: './userdashboard.component.html',
@@ -28,8 +28,14 @@ export class UserdashboardComponent {
   constructor(
     private auth: AuthService,
     private afAuth: AngularFireAuth,
-    private firestore: AngularFirestore
-  ) {}
+    private firestore: AngularFirestore,
+    private plateformlocaton: PlatformLocation
+  ) {
+    history.pushState(null, '', location.href);
+    this.plateformlocaton.onPopState(() => {
+      history.pushState(null, '', location.href);
+    });
+  }
 
   Logout() {
     this.auth.logout();
